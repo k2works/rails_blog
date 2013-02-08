80346167c2814a31e7304bdc1ea57e283cce8764
k2works <kakimomokuri@gmail.com>
2013-02-08 15:42:30 +0900

    Deleting Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,12 @@ class CommentsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
 		redirect_to post_path(@post)
-	end
+        end
+        
+        def destroy
+                @post =Post.find(params[:post_id])
+                @comment = @post.comments.find(params[:id])
+                @comment.destroy
+                redirect_to post_path(@post)
+        end
 end
